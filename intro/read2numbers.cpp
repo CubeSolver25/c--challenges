5183de640b4b8a89a1639dda0e85c8ac6a27733d
CubeSolver25 <36614761+CubeSolver25@users.noreply.github.com>
2019-02-10 12:54:42 -0800

    Create read2numbers.cpp

---
+++ intro/read2numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+  int num1, num2; 
+  double dnum1, dnum2;
+  std::string str;
+   num1 = 101000;
+   num2 = 100101001;
+   dnum1 = 978487934798;
+   dnum2 = 74334739897879324;
+   str = "8478676768746666746676268686863286489689798969678986767886725687697543678936678568959769786567896789569875786767889f56738uytr8eufiogdyiehurwjskfdyhiouqwjesyf8ouijq45whegrsutiuroeghu8oitjrgkfhduoijfdh8uoijt34yre7908u45ioweyrhouijtheriujo4w5hrfuijohesrfuidojkh3reuiojkmht3riueojsklhsupijah8quoijshe8oiu34wjeyh873u4iwojehij3o4kmwh379u4iowjhredsjkmghbirejkdhuncyhpuweijrsy78huiy47weiuskdhkjclwhesijdkf";
+   std::cout << num1+num2 << std::endl;
+   std::cout << dnum1+dnum2 << std::endl;
+   std::cout << str << std::endl;
+   std::cin >> num1;
+   std::cin >> num2;
+   std::cout << num1+num2 << std::endl;
+}
