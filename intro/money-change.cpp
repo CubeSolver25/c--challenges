da5f798c513e16263c9a828a8ef2159420a0f4a0
CubeSolver25 <36614761+CubeSolver25@users.noreply.github.com>
2019-03-10 13:48:35 -0700

    Update money-change.cpp

--- intro/money-change.cpp
+++ intro/money-change.cpp
@@ -2,44 +2,42 @@
 #include <iostream>
 #include <string>
 #include <math.h>
-#include <iostream>
 #include <sstream>
+#include <array>
+#include <sstream>
+
 
 
 //User inputs money, use datatype double (Ex. $1.23)
 //Give change in smallest amount of coins -> Use modulous operator to first remove all whole dollars (Ex. 1.23 % 1)
 //Use mod with 0.25, 0.1, 0.05, 0.01
 //return type  function  name(){}
-std::string change(double money);
-void testAverage(int arr[], int size);
+std::string change();
+void testAverage();
+
+
 int main()
 {
-    double money;
+    
     int counter;
     int menu;
-     int size; 
+
     bool keeploop = true;
     while(keeploop){
         std::cout << "1. Changer \n2. Test Average \n0. Exit"<< std::endl;
         std::cin >> menu;
     switch(menu){
-        case 1:
-        std::cout << "Money Changer: \nPlease enter the amount of money you have." << std::endl;
-        std::cin >> money;
-        std::cout<< change(money) << std::endl;
-        break;
-        case 2:
-        std::cin >> size;
-  int arr[size];
- for(int i = 0; i < size; i++){
-  std::cin >> arr[i];
-}
- testAverage(arr, size);
- break;
         case 0:
-        keeploop = false;
+         keeploop = false;
+         break;
+        case 1:
+        change();
         break;
-        default:
+        case 2: 
+        testAverage();
+        break;
+        case 4:
+      default:
         keeploop = false;
         break;
     }}
@@ -50,7 +48,10 @@ int main()
       //std::cout<< change(money) << std::endl;
 }
 
-std::string change(double money){
+std::string change(){
+  double money;
+   std::cout << "Money Changer: \nPlease enter the amount of money you have." << std::endl;
+        std::cin >> money;
     money += 0.001;
     unsigned long quarters, dimes, nickels = 0;
     quarters = dimes = nickels;
@@ -68,8 +69,15 @@ std::string change(double money){
     std::to_string(change) + " pennies.";
 return result;
 }
-void testAverage(int arr[], int size){
- 
+void testAverage(){
+      int size;
+    std::cout << "Test Average: \nPlease enter the number of test scores you want to average." << std::endl;
+std::cin >> size;
+  int arr[size];
+  for(int i = 0; i < size; i++){
+     std::cout << "Enter your score for Test #" << (i+1) <<  "." << std::endl;  
+  std::cin >> arr[i];
+  }
 int average = 0;
 for(int i = 0; i < size; i++){
 average += arr[i];}
