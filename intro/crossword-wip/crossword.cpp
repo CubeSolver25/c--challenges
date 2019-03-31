c42d223cef84d7a33a81b1cdd0562b4c706780b9
CubeSolver25 <36614761+CubeSolver25@users.noreply.github.com>
2019-03-31 14:27:14 -0700

    Add files via upload

--- intro/crossword-wip/crossword.cpp
+++ intro/crossword-wip/crossword.cpp
@@ -2,12 +2,30 @@
 #include <fstream>
 #include <string>
 using namespace std;
-int randomDirection();
+string randomDirection();
 char randomCharacter();
 void insertRandomCharacterToArray(char arr[][10], char W, int x, int y);
+void insertCharacterOfWordToArray(char arr[][10], string direction, string word);
+void insertCharacterToArray(char arr[][10], int x, int y, char character);
+bool checkBounds(char arr[][10], int x, int y, int w, int h);
+bool checkIfSpaceUsed(char arr[][10], int x, int y);
 int main(){
+    
     srand(time(0));
     char arr[10][10];
+      for(int i = 0; i < 10; i++){
+       for(int j = 0; j < 10; j++){
+  arr[i][j] = ' ';
+       }
+      }
+      for(int i = 0; i < 10; i++){
+       for(int j = 0; j < 10; j++){
+  checkIfSpaceUsed(arr, i, j);
+       }
+}
+    cout << sizeof(arr) << " " << sizeof(arr[0]) << " " << sizeof(arr[0][0]) << endl;
+    int w = sizeof(arr)/sizeof(arr[0]);
+    int h = sizeof(arr[0])/sizeof(arr[0][0]);
     int random[3];
     string randomWordlist[3];
     ifstream inFile;
@@ -35,7 +53,9 @@ int main(){
         cout << random[i] << endl;
         cout << randomWordlist[i] << endl;
     }
-
+for(int i = 0; i < 20; i++){
+randomDirection();
+}
    for(int i = 0; i < 10; i++){
        for(int j = 0; j < 10; j++){
   insertRandomCharacterToArray(arr, randomCharacter(), i, j);
@@ -43,16 +63,36 @@ int main(){
 }
    for(int i = 0; i < 10; i++){
        for(int j = 0; j < 10; j++){
-    cout << arr[i][j] << " ";
+    cout << arr[i][j] << "  ";
        }
     cout << endl;   
 }
 
-
+ for(int i = 0; i < 3; i++){
+      insertCharacterOfWordToArray(arr, randomDirection(), randomWordlist[i]);
+        
+    }
+      for(int i = 0; i < 10; i++){
+       for(int j = 0; j < 10; j++){
+  insertCharacterToArray(arr, i, j, 'z');
+       }
+}
+   for(int i = 0; i < 10; i++){
+       for(int j = 0; j < 10; j++){
+    cout << arr[i][j] << "  ";
+       }
+    cout << endl;   
+}
+    checkBounds(arr, 0, -1, w, h);
+      for(int i = 0; i < 10; i++){
+       for(int j = 0; j < 10; j++){
+  checkIfSpaceUsed(arr, i, j);
+       }
+}
     return 0;
 }
 
-int randomDirection(){
+string randomDirection(){
      int random;
      string direction;
         random = rand() % 4;
@@ -72,8 +112,8 @@ int randomDirection(){
       default:
         break;
     }
-    
-return 0;
+    cout << direction << endl;
+return direction;
 }
 
 char randomCharacter(){
@@ -89,4 +129,37 @@ void insertRandomCharacterToArray(char arr[][10], char W, int x, int y){
     arr[x][y] = W;
     cout << x << "-" << y << " Array Character: " << arr[x][y] << endl;
     return;
+}
+void insertCharacterOfWordToArray(char arr[][10], string direction, string word){
+ int x = rand() % 10;
+ int y = rand() % 10;  
+    arr[x][y] = word.at(0);
+    cout << word << " First Character: " << arr[x][y] << endl;
+}
+void insertCharacterToArray(char arr[][10], int x, int y, char character){
+    arr[x][y] = character;
+    cout << arr[x][y] << endl;
+}
+bool checkBounds(char arr[][10], int x, int y, int w, int h){
+cout << w << " " << h << endl;
+if( x > (w-1) || y > (h-1) || x < 0 || y < 0){
+cout << "The coordinate is out of bounds." << endl;
+    return true;
+}
+else
+{
+    cout << "The coordinate is inside the bounds." << endl;
+  return false;  
+}
+
+}
+bool checkIfSpaceUsed(char arr[][10], int x, int y){
+if(arr[x][y] != ' '){
+    cout << "This space is already occupied by another character." << endl;
+    return true;
+}
+    else{
+    cout << "This space is not occupied." << endl;
+    return false;
+    }
 }
